fd4ce93484ed552affc4f9873eaec448e73c6e7c
tungx1 <tungphamthanhcdt@gmail.com>
2023-12-04 17:18:26 +0700

    S

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Facebook from "expo-auth-session/providers/facebook";
+import * as WebBrowser from "expo-web-browser";
+import { Button } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [request, response, promptAsync] = Facebook.useAuthRequest({
+    clientId: "1484540205670419",
+  });
+  useEffect(() => {
+    if (response && response.type === "success" && response.authentication) {
+      (async () => {
+        const userInfoResponse = await fetch(
+          `https://graph.facebook.com/me?access_token=${response.authentication.accessToken}&fields=id,name,picture.type(large)`
+        );
+        const userInfo = await userInfoResponse.json();
+        setUser(userInfo);
+      })();
+    }
+  }, [response]);
+  const handlePressAsync = async () => {
+    const result = await promptAsync();
+    if (result.type !== "success") {
+      alert("something went wrong");
+      return;
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {user ? (
+        <Profile user={user} />
+      ) : (
+        <>
+          <Text>Login with facebook</Text>
+          <Button
+            disabled={!request}
+            title="Login with facebook"
+            onPress={handlePressAsync}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -13,8 +51,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+function Profile({ user }) {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>{user.name}</Text>
+    </View>
+  );
+}
